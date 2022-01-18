757c828ca00cae5e09cee00b6172f715c4be85b0
tamirgen <tamirgenkin@gmail.com>
2022-01-18 15:09:16 +0000

    Add a card

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,7 +1,6 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 if os.path.exists("env.py"):
     import env  # noqa
 
@@ -12,4 +11,4 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
-from taskmanager import routes   # noqa
+from taskmanager import routes  # noqa
--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,6 +1,5 @@
 from taskmanager import db
 
-
 class Category(db.Model):
     # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
